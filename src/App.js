7edad0974e78910275f09ef1340362cdfe5c25b4
Jan Korczak <jankorczak@Jans-MacBook-Air.local>
2023-05-22 00:17:27 +0200

    Login form done, need to work on errors and learning the concept

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return <div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" exact Component={Login}></Route>
+                <Route path="/register" exact Component={Register}></Route>
+            </Routes>
+        </BrowserRouter>
     </div>
-  );
 }
 
-export default App;
+export default App
---
+++ src/pages/Register.js
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function registerUser(ev) {
+    ev.preventDefault();
+    const response = await fetch('http://localhost:1337/api/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      }, 
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    })
+
+    const data = await response.json();
+
+    console.log(data);
+  }
+
+  return (
+    <div>
+      <h1>Register</h1>
+      <form onSubmit={registerUser}>
+        <input value={name} onChange={(ev) => setName(ev.target.value)} type="text" placeholder="First Name" />
+        <input value={email} onChange={(ev) => setEmail(ev.target.value)} type="email" placeholder="Email" />
+        <input value={password} onChange={(ev) => setPassword(ev.target.value)} type="password" placeholder="Password" />
+        <input type="submit" value="Register" />
+      </form>
+    </div>
+  );
+}
+
+export default App;
